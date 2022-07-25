61ffe252ed9ba5de4d27b1efcf0a6b160c028b84
nathanlerit1991 <nathan.lerit@gmail.com>
2022-07-25 19:44:07 +0800

    update

--- src/.vuepress/theme/layouts/Lobby.vue
+++ src/.vuepress/theme/layouts/Lobby.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
+    <Games :games="$page.frontmatter.game" />
     <Content/>
   </div>
 </template>
 
 <script>
-
+import Games from '../../components/Games'
 export default {
+  components: {
+    Games
+  },
   data () {
     return {
-      
+
     }
   }
 }
