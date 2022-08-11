459a9a053983d6dcfce2c05636e57a8c23d762b1
nathanlerit1991 <nathan.lerit@gmail.com>
2022-08-11 22:48:54 +0800

    added vjfp

--- src/.vuepress/theme/layouts/VJFrontPage.vue
+++ src/.vuepress/theme/layouts/VJFrontPage.vue
@@ -35,6 +35,43 @@
 				</div>
 			</div>
 		</section>
+		<section id="s-games">
+			<div class="container">
+
+				<div class="row txt-center">
+					<div v-html="$page.frontmatter.our_games.text_content" />
+				</div>
+
+				<div :class="'row column-' + $page.frontmatter.our_games.top_games.length" class="game-list">
+					<div v-for="(games, gamesIndex) in $page.frontmatter.our_games.top_games" :key="gamesIndex">
+						<h3>{{ games.title }}</h3>
+						<p>{{ games.description }}</p>
+						<ol>
+							<li v-for="(listGames, listGamesIndex) in games.list_top_games" :key="listGamesIndex">
+								<span v-if="typeof listGames.url !== 'undefined' && listGames.url !== ''" @click="modal = true, modalData = listGames.url, gameTitle = listGames.name">
+									<img class="lazy" :src="listGames.image" />
+									{{ listGames.name }}
+								</span>
+								<p v-else>
+									<img class="lazy" :src="listGames.image" />
+									{{ listGames.name }}
+								</p>
+							</li>
+						</ol>
+					</div>
+				</div>
+				<div class="row h-center">
+					<button data-test-id="registration-button" class="btn-primary">
+						<a :href="$page.frontmatter.our_games.cta_link">
+							{{ $page.frontmatter.our_games.cta_text }}
+						</a>
+					</button>
+				</div>
+				<div class="row h-center">
+					<small v-html="$page.frontmatter.our_games.text_hyperlink" />
+				</div>
+			</div>
+		</section>
 	</div>
 </template>
 
