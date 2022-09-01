2ceb409b275c34aa4265858170f4ec147c90d62e
nathanlerit1991 <nathan.lerit@gmail.com>
2022-09-01 20:42:10 +0800

    test

--- src/.vuepress/theme/layouts/VJFrontPage.vue
+++ src/.vuepress/theme/layouts/VJFrontPage.vue
@@ -226,20 +226,18 @@
 
 		<section id="s-other-info" class="content-visibility">
 			<div class="container no-container">
-				<div class="row">
-					<div>
-						<div v-html="$page.frontmatter.other_info.title" />
-						<ul>
-							<li v-for="(dataInfo, dataInfoindex) in $page.frontmatter.other_info.list_text_links" :key="dataInfoindex">
-								<a :href="dataInfo.text_url">{{ dataInfo.text }}</a>
-							</li>
-						</ul>
-						<div class="footer-twitter-button-pure">
-							<a :href="$page.frontmatter.twitter_link">
-								<img class="lazy" src="/assets/verajohn/Vjfp/twitter.svg" alt="twitter"/>
-								<span>{{ $page.frontmatter.twitter_title }}</span>
-							</a>
-						</div>
+				<div>
+					<div v-html="$page.frontmatter.other_info.title" />
+					<ul>
+						<li v-for="(dataInfo, dataInfoindex) in $page.frontmatter.other_info.list_text_links" :key="dataInfoindex">
+							<a :href="dataInfo.text_url">{{ dataInfo.text }}</a>
+						</li>
+					</ul>
+					<div class="footer-twitter-button-pure">
+						<a :href="$page.frontmatter.twitter_link">
+							<img class="lazy" src="/assets/verajohn/Vjfp/twitter.svg" alt="twitter"/>
+							<span>{{ $page.frontmatter.twitter_title }}</span>
+						</a>
 					</div>
 				</div>
 			</div>
