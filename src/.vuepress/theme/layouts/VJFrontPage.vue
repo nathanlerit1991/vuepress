8cffddec0fd1318f97aea4f429a313aab6539ee2
nathanlerit1991 <nathan.lerit@gmail.com>
2022-09-02 10:36:03 +0800

    remove computed background

--- src/.vuepress/theme/layouts/VJFrontPage.vue
+++ src/.vuepress/theme/layouts/VJFrontPage.vue
@@ -1,10 +1,5 @@
 <template>
-	<div class="vjfp" 
-		:style="
-		`--main-bg: url(${$page.frontmatter.vjfp_bg});` +
-		`--main-bg-mobile: url(${$page.frontmatter.welcome_verajohn.mobile_bg_image});` +
-		`--customer-support-background-image: url(${$page.frontmatter.customer_support.bg_image})`
-		">
+	<div class="vjfp">
 
 
 		<Modal 
