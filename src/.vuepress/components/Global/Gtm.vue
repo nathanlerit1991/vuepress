cf0434c17a40e641af54542c9c025205a52efce3
nathanlerit1991 <nathan.lerit@gmail.com>
2022-10-27 21:47:59 +0800

    create components for GTM meta data

--- src/.vuepress/components/Global/Gtm.vue
+++ src/.vuepress/components/Global/Gtm.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe loading="lazy" src="https://www.googletagmanager.com/ns.html?id=GTM-KCLSQC8" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+  <iframe loading="lazy" src="https://www.googletagmanager.com/ns.html?id=G-ET0XZ56ZLJ" height="0" width="0" style="display:none;visibility:hidden"></iframe>
 </template>
 <script>
 export default {
@@ -18,7 +18,7 @@ export default {
   			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-KCLSQC8');
+      })(window,document,'script','dataLayer','G-ET0XZ56ZLJ');
     }
     gtmHead.insertBefore(script, gtmHead.firstChild)
 	}
