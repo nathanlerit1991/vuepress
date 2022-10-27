81eaef4def3ce108044f48e904d6471857275be2
nathanlerit1991 <nathan.lerit@gmail.com>
2022-10-27 22:51:23 +0800

    fix gtm

--- src/.vuepress/components/Global/Gtm.vue
+++ src/.vuepress/components/Global/Gtm.vue
@@ -4,23 +4,23 @@
 <script>
 export default {
 	mounted () {
-		// let head = document.head
-		// let gtmHead = document.getElementsByTagName("head")[0]
-		// let script = document.createElement('script')
-		// script.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-KCLSQC8'
-		// gtmHead.insertBefore(script, gtmHead.firstChild)
+		let head = document.head
+		let goHead = document.getElementsByTagName("head")[0]
+		let goScript = document.createElement('script')
+		goScript.src = 'https://www.googleoptimize.com/optimize.js?id=OPT-K6FTGKG'
+		goHead.insertBefore(goScript, goHead.firstChild)
 
 
     let gtmHead = document.getElementsByTagName("head")[0]
-    let script = document.createElement('script')
-    script.innerHTML = function handleScriptLoaded() {
+    let gtmScript = document.createElement('script')
+    gtmScript.innerHTML = function handleScriptLoaded() {
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','OPT-K6FTGKG');
     }
-    gtmHead.insertBefore(script, gtmHead.firstChild)
+    gtmHead.insertBefore(gtmScript, gtmHead.firstChild)
 	}
 }
 </script>
