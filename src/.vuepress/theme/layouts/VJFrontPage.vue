44f153a3f64e67bef758d84b64936cf7553011ac
nathanlerit1991 <nathan.lerit@gmail.com>
2022-11-02 10:09:33 +0800

    remove meta data

--- src/.vuepress/theme/layouts/VJFrontPage.vue
+++ src/.vuepress/theme/layouts/VJFrontPage.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="vjfp" class="lazy-background" :data-style="`--customer-support-background-image: url('${$page.frontmatter.customer_support.bg_image}');`">
 		<!-- <link v-if="$page.frontmatter.layout === 'VJFrontPage'" href="/css/index.css" rel="stylesheet"/> -->
-		<MetaData />
+		<!-- <MetaData /> -->
 		<!-- <TrackingScript /> -->
 		<!-- BACKGROUND IMAGES ABOVE THE FOLD / REDUCE LCP -->
 		<picture class="above-fold-bg">
