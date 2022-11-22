73abe85e13ff49e675436f2423d006a08298edcb
nathanlerit1991 <nathan.lerit@gmail.com>
2022-11-22 20:44:01 +0800

    sample

---
+++ src/.vuepress/theme/layouts/sample.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="above-fold-sample">
+      <img v-for="(data, data_index) in 5" :key="data_index" :src="'/assets/sample/' + (data_index + 1) + '.png'"/>
+    </div>
+
+    <div v-for="(data, data_index) in 100" :key="data_index">
+      <img :src="'/assets/sample/' + (data_index + 6) + '.png'" loading="lazy"/>
+    </div>
+  </div>
+</template>
+
+<style>
+.above-fold-sample {
+  min-height: 100vh;
+}
+</style>
